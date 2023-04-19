9e150179dc57120f0480463cf89be96496ebf459
Max Segal <Serenity09@users.noreply.github.com>
2023-04-18 21:39:33 -0700

    Schema cleanup
    Removed columns from schema that are no longer defined on the scraper

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,11 @@ const medicaidTableURL = 'https://www.medicaid.gov/medicaid/national-medicaid-ch
 
 type TableHTML = {
   parsedString: string;
-  parsedPercent: number | null;
   footnoteID: string | null;
 }
 
-type TableHeaderHTML = TableHTML & {
-  ageLowerBound: number;
-  ageUpperBound: number | null;
-  appliesToIndividuals: boolean;
-  appliesWhenPregnant: boolean;
+type TableCellHTML = TableHTML & {
+  parsedPercent: number | null;
 }
 
 const parseTableHTML = function(str: string): TableHTML {
@@ -29,16 +25,7 @@ const parseTableHTML = function(str: string): TableHTML {
     }
   }
   catch (err) { console.log(err) }
-  
-  let parsedPercent = null;
-  try {
-    const percentIndex = str.indexOf("%");
-    if (percentIndex >= 0) {
-      parsedPercent = parseFloat(str.substring(0, percentIndex)) / 100.;
-    }
-  }
-  catch (err) { console.log(err) }
-  
+    
   let footnoteID = null;
   try {
     const footnoteStartIndex = str.search(/>[0-9]+</);
@@ -51,76 +38,40 @@ const parseTableHTML = function(str: string): TableHTML {
 
   return {
     parsedString,
-    parsedPercent,
     footnoteID
   }
 }
-const parseTableHeaderHTML = function(str: string): TableHeaderHTML {
-  const parsedHTML = parseTableHTML(str) as TableHeaderHTML;
-
-  let parsedAgeLowerBound: number = 19;
-  let parsedAgeUpperBound: number | null = null;
+const parseTableCellHTML = function(str: string): TableCellHTML {
+  let parsedPercent = null;
   try {
-    if (str.indexOf("Children") >= 0) {
-      const ageHypenIndex = str.indexOf("-", str.indexOf("Ages"));
-      
-      if (ageHypenIndex >= 0) {
-        let ageLowerBoundIndex = ageHypenIndex - 1;
-        while (str.charAt(ageLowerBoundIndex) != " ") {
-          ageLowerBoundIndex--;
-        }
-        parsedAgeLowerBound = parseInt(str.substring(ageLowerBoundIndex, ageHypenIndex));
-
-        let ageUpperBoundIndex = ageHypenIndex + 1;
-        while (str.charAt(ageUpperBoundIndex) != "<") {
-          ageUpperBoundIndex++;
-        }
-        parsedAgeUpperBound = parseInt(str.substring(ageHypenIndex + 1, ageUpperBoundIndex));
-      }
-      else if (str.indexOf("CHIP") >= 0) {
-        parsedAgeLowerBound = 0;
-        parsedAgeUpperBound = 19;
-      }
+    const percentIndex = str.indexOf("%");
+    if (percentIndex >= 0) {
+      parsedPercent = parseFloat(str.substring(0, percentIndex)) / 100.;
     }
   }
   catch (err) { console.log(err) }
 
-  parsedHTML.ageLowerBound = parsedAgeLowerBound;
-  parsedHTML.ageUpperBound = parsedAgeUpperBound;
-
-  let parsedAppliesToIndividuals: boolean = true;
-  let parsedAppliesWhenPregnant: boolean = false;
-  try {
-    if (str.indexOf("Parent") >= 0 || str.indexOf("Caretaker") >= 0) {
-      parsedAppliesToIndividuals = false;
-      parsedAppliesWhenPregnant = true;
-    }
-    else if (str.indexOf("Pregnant") >= 0) {
-      parsedAppliesWhenPregnant = true;
-    }
+  const parsedHTML = parseTableHTML(str);
+  return {
+    parsedPercent,
+    ...parsedHTML
   }
-  catch (err) { console.log(err) }
-
-  parsedHTML.appliesToIndividuals = parsedAppliesToIndividuals;
-  parsedHTML.appliesWhenPregnant = parsedAppliesWhenPregnant;
-
-  return parsedHTML;
 }
 
 const parseMedicaidTableToJson = function(medicaidTable: HTMLElement) {
-  let columns: TableHeaderHTML[] | null = null;
+  let columns: TableHTML[] | null = null;
   try {
     const headerRow = medicaidTable.childNodes[0];
-    columns = headerRow.childNodes.filter((headerEle) => headerEle.nodeType === 1).map((headerEle: any) => parseTableHeaderHTML(headerEle.innerHTML));
+    columns = headerRow.childNodes.filter((headerEle) => headerEle.nodeType === 1).map((headerEle: any) => parseTableHTML(headerEle.innerHTML));
   }
   catch (err) { console.log(err); }
 
-  let data: TableHTML[][] = [];
+  let data: TableCellHTML[][] = [];
   try {
     const [, ...dataRows] = medicaidTable.childNodes;
 
     for (let dataRow of dataRows) {
-      data.push(dataRow.childNodes.filter((headerEle) => headerEle.nodeType === 1).map((headerEle: any) => parseTableHTML(headerEle.innerHTML)));
+      data.push(dataRow.childNodes.filter((headerEle) => headerEle.nodeType === 1).map((headerEle: any) => parseTableCellHTML(headerEle.innerHTML)));
     }
   }
   catch (err) { console.log(err); }
@@ -131,6 +82,7 @@ const parseMedicaidTableToJson = function(medicaidTable: HTMLElement) {
   };
 }
 
+//TODO needs to be updated for latest page layout
 const parseFootnoteListToJson = function(footnoteList: HTMLElement) {
   let footnotes: any[] = [];
 
